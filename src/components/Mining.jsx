72f3850b934553ca92d207c56e7408056d1651d1
havenprotocol <havencurrency@gmail.com>
2018-02-27 23:09:50 +1300

    update pool list

--- src/components/Mining.jsx
+++ src/components/Mining.jsx
@@ -8,8 +8,11 @@ class Mining extends Component {
       <div className="mining">
         <p>Choose from one of the Haven mining pools below:</p>
         <br />
-        <a className="pool-btn" target="_blank" href="http://pool.mineallcrypto.com/pool/?symbol=XHV">mineAllCrypto</a>
-        <a className="pool-btn" target="_blank" href="https://xhv.luckypool.io/">luckypool</a>
+        <a className="pool-btn" target="_blank" href="http://cryptoknight.cc/haven/">cryptoknight.cc</a>
+        <a className="pool-btn" target="_blank" href="http://pool.mineallcrypto.com/pool/?symbol=XHV">mineAllCrypto.com</a>
+        <a className="pool-btn" target="_blank" href="https://xhv.luckypool.io/">luckypool.io</a>
+        <a className="pool-btn" target="_blank" href="https://haven.miner.rocks/">haven.miner.rocks</a>
+        <a className="pool-btn" target="_blank" href="https://monerise.com/?pool=haven">monerise.com</a>
       </div>
     );
   }
