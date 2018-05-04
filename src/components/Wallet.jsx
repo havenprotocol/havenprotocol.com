4b5dfb4d3f39e4fd7e093f8c6f7691f495520688
havenprotocol <havencurrency@gmail.com>
2018-05-05 06:50:33 +1200

    update wallet links

--- src/components/Wallet.jsx
+++ src/components/Wallet.jsx
@@ -9,22 +9,19 @@ class Wallet extends Component {
         <div className="wallet-row">
           <div className="item">
             <i className="fab fa-windows"></i>
-            <a href="https://github.com/havenprotocol/haven/releases/download/2.0.0/haven-gui-windows-2.1.0.zip">
+            <a href="https://github.com/havenprotocol/haven/releases/download/2.1.0/haven-gui-windows-2.1.0.zip">
                 <div>Download Windows GUI + CLI</div>
             </a>
           </div>
           <div className="item">
             <i className="fab fa-apple"></i>
-            <a href="https://github.com/havenprotocol/haven/releases/download/2.0.0/haven-gui-mac-osx-2.0.0.zip">
+            <a href="https://github.com/havenprotocol/haven/releases/download/2.1.0/haven-gui-mac-osx-2.1.0.zip">
                 <div>Download OSX GUI</div>
-            </a>
-            <a href="https://github.com/havenprotocol/haven/releases/download/2.0.0/haven-cli-mac-osx-2.0.0.zip">
-                <div>Download OSX CLI</div>
             </a>
           </div>
           <div className="item">
             <i className="fab fa-linux"></i>
-            <a href="https://github.com/havenprotocol/haven/releases/download/2.0.0/haven-cli-linux-x64-2.0.1.zip">
+            <a href="https://github.com/havenprotocol/haven/releases/download/2.1.0/haven-cli-linux-x64-2.1.0.zip">
                 <div>Download Linux CLI</div>
             </a>
           </div>
