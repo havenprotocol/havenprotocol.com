cc48f522608e9a3cc94025bc4c9d66fde1f87bb7
havenprotocol <havencurrency@gmail.com>
2018-05-15 22:51:11 +1200

    update links

--- src/components/Links.jsx
+++ src/components/Links.jsx
@@ -8,17 +8,19 @@ class Mining extends Component {
       <div className="links">
         <div className="category">
           <p className="heading">Mining Pools</p>
-          <a target="_blank" href="http://cryptoknight.cc/haven/">cryptoknight.cc</a>
+	  <a target="_blank" href="https://pool.havenprotocol.com/"><b>pool.havenprotocol.com</b></a>
+	  <a target="_blank" href="http://cryptoknight.cc/haven/">cryptoknight.cc</a>
           <a target="_blank" href="http://pool.mineallcrypto.com/pool/?symbol=XHV">mineAllCrypto.com</a>
           <a target="_blank" href="https://xhv.luckypool.io/">luckypool.io</a>
           <a target="_blank" href="https://haven.miner.rocks/">haven.miner.rocks</a>
-          <a target="_blank" href="http://www.gemtank.ca/">gemtank.ca</a>
+          <a target="_blank" href="https://xhv.dark-mine.su/">xhv.dark-mine.su</a>
           <a target="_blank" href="https://haven.herominers.com/">herominers.com</a>
         </div>
         <div className="category">
           <p className="heading">Exchanges</p>
           <a target="_blank" href="https://tradeogre.com/exchange/BTC-XHV">TradeOgre</a>
           <a target="_blank" href="https://nanex.co/exchange/XHVNANO">Nanex</a>
+	  <a target="_blank" href="https://altex.exchange/markets&pair=BTC_XHV">Altex</a>
           <a target="_blank" href="https://palitanx.com/exchange/BTC-XHV">Palitanx</a>
         </div>
         <div className="category">
