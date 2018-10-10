3ac25ad2ee7b08173e450dba93e17a42dc73f897
havenprotocol <havencurrency@gmail.com>
2018-10-10 14:09:27 +0200

    update links

--- src/components/Links.jsx
+++ src/components/Links.jsx
@@ -8,20 +8,18 @@ class Mining extends Component {
       <div className="links">
         <div className="category">
           <p className="heading">Mining Pools</p>
-	        <a target="_blank" href="https://pool.havenprotocol.com/"><b>pool.havenprotocol.com</b></a>
           <a target="_blank" href="https://xhv.luckypool.io/">luckypool.io</a>
+	        <a target="_blank" href="https://pool.havenprotocol.com/">pool.havenprotocol.com</a>
           <a target="_blank" href="https://haven.hashvault.pro/">haven.hashvault.pro</a>
           <a target="_blank" href="https://haven.herominers.com/">herominers.com</a>
           <a target="_blank" href="https://xhv.fairpool.xyz/">fairpool.xyz</a>
-          <a target="_blank" href="https://haven.miner.rocks/">haven.miner.rocks</a>
           <a target="_blank" href="http://pool.haven.frackingminer.com/">pool.haven.frackingminer.com</a>
         </div>
         <div className="category">
           <p className="heading">Exchanges</p>
+          <a target="_blank" href="https://bittrex.com/Market/Index?MarketName=BTC-XHV">Bittrex</a>
+          <a target="_blank" href="https://upbit.com/exchange?code=CRIX.UPBIT.BTC-XHV">Upbit</a>
           <a target="_blank" href="https://tradeogre.com/exchange/BTC-XHV">TradeOgre</a>
-          <a target="_blank" href="https://www.southxchange.com/Market/Book/XHV/BTC">SouthXchange</a>
-          <a target="_blank" href="https://altex.exchange/markets&pair=BTC_XHV">Altex</a>
-          <a target="_blank" href="https://palitanx.com/exchange/BTC-XHV">Palitanx</a>
         </div>
         <div className="category">
           <p className="heading">Other</p>
